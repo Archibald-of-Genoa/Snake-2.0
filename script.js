9f5aedde7de34752eabcb4fe1daad026a4b3fdaf
Artem Chernov <archibaldofgenoa@yandex.ru>
2023-11-23 18:17:26 +0300

    Pivate and Public methods created

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ class GameField {
   constructor(rows, columns) {
     this.rows = rows;
     this.columns = columns;
-    this.gameField = document.getElementById("gameField");
-    this.center = {
+    this._gameField = document.getElementById("gameField");
+    this._center = {
       x: Math.floor(columns / 2),
       y: Math.floor(rows / 2),
     };
-    this.createGameField();
+    this._createGameField();
   }
 
-  createGameField() {
-    this.gameField.style.gridTemplateColumns = `repeat(${this.columns}, 1fr)`;
+  _createGameField() {
+    this._gameField.style.gridTemplateColumns = `repeat(${this.columns}, 1fr)`;
     for (let i = 0; i < this.columns * this.rows; i++) {
       const outerSquare = document.createElement("div");
       outerSquare.classList.add("outerSquare");
@@ -20,71 +20,70 @@ class GameField {
 
       outerSquare.appendChild(innerSquare);
 
-      this.gameField.appendChild(outerSquare);
+      this._gameField.appendChild(outerSquare);
     }
   }
 }
 
 class Snake {
   constructor(gameField) {
-    this.gameField = gameField;
-
-    const centerX = gameField.center.x;
-    const centerY = gameField.center.y;
-
-    this.body = [
+    this._gameField = gameField;
+    const centerX = gameField._center.x;
+    const centerY = gameField._center.y;
+    this._body = [
       {
         x: centerX,
         y: centerY,
       },
-
       { x: centerX - 1, y: centerY },
     ];
-
-    this.direction = "right";
-
-    this.snakeRendering();
-
-    this.score = 0;
-    this.highScore = localStorage.getItem("snakeHighScore") || 0;
-
-    this.updateScoreDisplay();
+    this._direction = "right";
+    this._snakeRendering();
+    this._score = 0;
+    this._highScore =
+      localStorage.getItem("snakeHighScore") || 0;
+    this._updateScoreDisplay();
   }
 
-  isEatingApple(applePosition) {
-    const head = this.body[0];
-    const isEating = head.x === applePosition.x && head.y === applePosition.y;
+  _isEatingApple(applePosition) {
+    const head = this._body[0];
+    const isEating =
+      head.x === applePosition.x && head.y === applePosition.y;
 
     if (isEating) {
-      this.score += 10;
-      if (this.score > this.highScore) {
-        this.highScore = this.score;
-        localStorage.setItem("snakeHighScore", this.highScore);
+      this._score += 10;
+      if (this._score > this._highScore) {
+        this._highScore = this._score;
+        localStorage.setItem("snakeHighScore", this._highScore);
       }
-      this.updateScoreDisplay();
+      this._updateScoreDisplay();
     }
 
     return isEating;
   }
 
-  updateScoreDisplay() {
+  _updateScoreDisplay() {
     const scoreNums = document.querySelector(".scoreNums");
     const hiScoreNums = document.querySelector(".hiScoreNums");
 
     if (scoreNums && hiScoreNums) {
-      scoreNums.textContent = this.score;
-      hiScoreNums.textContent = this.highScore;
+      scoreNums.textContent = this._score;
+      hiScoreNums.textContent = this._highScore;
     }
   }
 
-  snakeRendering() {
+  _snakeRendering() {
     const fieldElement = document.getElementById("gameField");
-    this.clearSnakeFromField();
+    this._clearSnakeFromField();
 
-    this.body.forEach((segment, index) => {
-      const cellIndex = segment.y * gameField.columns + segment.x;
+    this._body.forEach((segment, index) => {
+      const cellIndex =
+        segment.y * this._gameField.columns + segment.x;
 
-      if (cellIndex >= 0 && cellIndex < fieldElement.children.length) {
+      if (
+        cellIndex >= 0 &&
+        cellIndex < fieldElement.children.length
+      ) {
         const cell = fieldElement.children[cellIndex];
         cell.classList.add("snake");
         const innerSquare = cell.children[0];
@@ -93,7 +92,7 @@ class Snake {
     });
   }
 
-  clearSnakeFromField() {
+  _clearSnakeFromField() {
     const fieldElement = document.getElementById("gameField");
     for (let i = 0; i < fieldElement.children.length; i++) {
       const cell = fieldElement.children[i];
@@ -101,30 +100,32 @@ class Snake {
     }
   }
 
-  checkCollisionWithSelf() {
-    const head = this.body[0];
-    for (let i = 1; i < this.body.length; i++) {
-      if (head.x === this.body[i].x && head.y === this.body[i].y) {
-        apple.clearAppleFromField();
+  _checkCollisionWithSelf() {
+    const head = this._body[0];
+    for (let i = 1; i < this._body.length; i++) {
+      if (head.x === this._body[i].x && head.y === this._body[i].y) {
+        apple._clearAppleFromField();
         return true;
       }
     }
     return false;
   }
 
-  moving() {
-    const head = this.body[0];
+  _moving() {
+    const head = this._body[0];
 
-    if (this.checkCollisionWithSelf()) {
-      alert("Опаньки!!! Кажется, змей укусил сам себя за яйку =( ");
+    if (this._checkCollisionWithSelf()) {
+      alert(
+        "Опаньки!!! Кажется, змей укусил сам себя за яйку =( "
+      );
       runAnimation();
-      this.resetGame();
+      this._resetGame();
       return;
     }
 
     let newHead;
 
-    switch (this.direction) {
+    switch (this._direction) {
       case "up":
         newHead = { x: head.x, y: head.y - 1 };
         break;
@@ -142,140 +143,134 @@ class Snake {
     }
 
     if (newHead.x < 0) {
-      newHead.x = this.gameField.columns - 1;
-    } else if (newHead.x >= this.gameField.columns) {
+      newHead.x = this._gameField.columns - 1;
+    } else if (newHead.x >= this._gameField.columns) {
       newHead.x = 0;
     }
 
     if (newHead.y < 0) {
-      newHead.y = this.gameField.rows - 1;
-    } else if (newHead.y >= this.gameField.rows) {
+      newHead.y = this._gameField.rows - 1;
+    } else if (newHead.y >= this._gameField.rows) {
       newHead.y = 0;
     }
 
-    this.body.unshift(newHead);
+    this._body.unshift(newHead);
 
-    if (this.isEatingApple(apple.position)) {
-      apple.clearAppleFromField();
-      apple.position = apple.generateApplePosition();
-      apple.appleRendering();
+    if (this._isEatingApple(apple._position)) {
+      apple._clearAppleFromField();
+      apple._position = apple._generateApplePosition();
+      apple._appleRendering();
     } else {
-      if (this.body.length > 1) {
-        const tail = this.body.pop();
-        const tailIndex = tail.y * this.gameField.columns + tail.x;
+      if (this._body.length > 1) {
+        const tail = this._body.pop();
+        const tailIndex =
+          tail.y * this._gameField.columns + tail.x;
         const tailCell =
-          document.getElementById("gameField").children[tailIndex];
+          document.getElementById("gameField").children[
+            tailIndex
+          ];
         tailCell.classList.remove("snake");
         tailCell.classList.remove("apple");
       }
     }
 
-    this.snakeRendering();
+    this._snakeRendering();
   }
 
-  isEatingApple(applePosition) {
-    const head = this.body[0];
-    const isEating = head.x === applePosition.x && head.y === applePosition.y;
+  _resetGame() {
+    this._clearSnakeFromField();
 
-    if (isEating) {
-      this.score += 10;
-      if (this.score > this.highScore) {
-        this.highScore = this.score;
-        localStorage.setItem("snakeHighScore", this.highScore);
-      }
-      this.updateScoreDisplay();
-    }
-
-    return isEating;
-  }
-
-  resetGame() {
-    this.clearSnakeFromField();
-
-    const centerX = this.gameField.center.x;
-    const centerY = this.gameField.center.y;
-    this.body = [
+    const centerX = this._gameField._center.x;
+    const centerY = this._gameField._center.y;
+    this._body = [
       { x: centerX, y: centerY },
       { x: centerX - 1, y: centerY },
     ];
-    this.direction = "right";
+    this._direction = "right";
 
-    this.score = 0;
-    this.updateScoreDisplay();
+    this._score = 0;
+    this._updateScoreDisplay();
 
-    apple.position = apple.generateApplePosition();
-    this.snakeRendering();
-    apple.appleRendering();
+    apple._position = apple._generateApplePosition();
+    this._snakeRendering();
+    apple._appleRendering();
   }
 
-  listeningForKeydown() {
-    document.addEventListener("keydown", this.handleKeyDown.bind(this));
+  _listeningForKeydown() {
+    document.addEventListener(
+      "keydown",
+      this._handleKeyDown.bind(this)
+    );
   }
 
-  handleKeyDown(event) {
+  _handleKeyDown(event) {
     switch (event.key) {
       case "ArrowUp":
-        this.setDirection("up");
+        this._setDirection("up");
         break;
       case "ArrowDown":
-        this.setDirection("down");
+        this._setDirection("down");
         break;
       case "ArrowLeft":
-        this.setDirection("left");
+        this._setDirection("left");
         break;
       case "ArrowRight":
-        this.setDirection("right");
+        this._setDirection("right");
         break;
     }
   }
 
-  setDirection(newDirection) {
+  _setDirection(newDirection) {
     if (
-      (newDirection === "up" && this.direction !== "down") ||
-      (newDirection === "down" && this.direction !== "up") ||
-      (newDirection === "left" && this.direction !== "right") ||
-      (newDirection === "right" && this.direction !== "left")
+      (newDirection === "up" && this._direction !== "down") ||
+      (newDirection === "down" && this._direction !== "up") ||
+      (newDirection === "left" && this._direction !== "right") ||
+      (newDirection === "right" && this._direction !== "left")
     ) {
-      this.direction = newDirection;
+      this._direction = newDirection;
     }
   }
 }
 
 class Apple {
   constructor(gameField) {
-    this.gameField = gameField;
-    this.position = this.generateApplePosition();
+    this._gameField = gameField;
+    this._position = this._generateApplePosition();
   }
 
-  generateApplePosition() {
+  _generateApplePosition() {
     while (true) {
       const position = {
-        x: Math.floor(Math.random() * gameField.rows),
-        y: Math.floor(Math.random() * gameField.columns),
+        x: Math.floor(Math.random() * this._gameField.rows),
+        y: Math.floor(Math.random() * this._gameField.columns),
       };
 
-      if (!this.isAppleOnSnake(position)) {
+      if (!this._isAppleOnSnake(position)) {
         return position;
       }
     }
   }
 
-  isAppleOnSnake(position) {
-    return snake.body.some((segment) => {
-      return segment.x === position.x && segment.y === position.y;
+  _isAppleOnSnake(position) {
+    return snake._body.some((segment) => {
+      return (
+        segment.x === position.x && segment.y === position.y
+      );
     });
   }
 
-  appleRendering() {
+  _appleRendering() {
     const fieldElement = document.getElementById("gameField");
-    const cellIndex = this.position.y * gameField.columns + this.position.x;
+    const cellIndex =
+      this._position.y * this._gameField.columns + this._position.x;
     const cell = fieldElement.children[cellIndex];
     cell.classList.add("apple");
   }
 
-  clearAppleFromField() {
+  _clearAppleFromField() {
     const fieldElement = document.getElementById("gameField");
-    const cellIndex = this.position.y * gameField.columns + this.position.x;
+    const cellIndex =
+      this._position.y * this._gameField.columns + this._position.x;
     const cell = fieldElement.children[cellIndex];
     cell.classList.remove("apple");
   }
@@ -284,34 +279,29 @@ class Apple {
 const gameField = new GameField(30, 30);
 
 const snake = new Snake(gameField);
-snake.listeningForKeydown();
+snake._listeningForKeydown();
 
 const apple = new Apple(gameField);
-apple.appleRendering();
+apple._appleRendering();
 
 setInterval(() => {
-  snake.moving();
+  snake._moving();
 }, 100);
 
-document.getElementById("resetBtn").addEventListener("click", handleResetClick);
+document.getElementById("resetBtn").addEventListener("click", _handleResetClick);
 
-async function handleResetClick() {
-  // Запускаем анимацию
-  await runAnimation();
-
-  // Даем задержку перед сбросом яблока и игры
+async function _handleResetClick() {
+  await _runAnimation();
   await new Promise((resolve) => setTimeout(resolve, 300));
-
-  // Теперь сбрасываем яблоко и игру
-  apple.clearAppleFromField();
-  snake.resetGame();
+  apple._clearAppleFromField();
+  snake._resetGame();
 }
 
-async function runAnimation() {
+async function _runAnimation() {
   const cells = document.querySelectorAll(".outerSquare");
   const rows = gameField.rows;
 
-  function colorRow(rowIndex) {
+  function _colorRow(rowIndex) {
     const currentRowIndexes = Array.from(
       { length: gameField.columns },
       (_, index) => rowIndex * gameField.columns + index
@@ -322,7 +312,7 @@ async function runAnimation() {
     });
   }
 
-  function resetRow(rowIndex) {
+  function _resetRow(rowIndex) {
     const currentRowIndexes = Array.from(
       { length: gameField.columns },
       (_, index) => rowIndex * gameField.columns + index
@@ -336,7 +326,7 @@ async function runAnimation() {
   let currentRow = rows - 1;
 
   const intervalId = setInterval(() => {
-    colorRow(currentRow);
+    _colorRow(currentRow);
 
     currentRow--;
 
@@ -345,7 +335,7 @@ async function runAnimation() {
 
       let resetRowInterval = setInterval(() => {
         if (currentRow >= 0 && currentRow < rows) {
-          resetRow(currentRow);
+          _resetRow(currentRow);
         }
 
         currentRow++;
